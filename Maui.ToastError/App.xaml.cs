19ec57fdd922f4be3e1978bc6925a9ab27aab280
Daniel Ancines <daniel.ancines@gmail.com>
2023-02-02 16:18:55 -0300

    Add project files.

---
+++ Maui.ToastError/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.ToastError;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui.ToastError/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace Maui.ToastError;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			await Toast.Make("Toast Test").Show();
+		}
+		catch (Exception ex)
+		{
+			_= Shell.Current.DisplayAlert("Error", $"{ex.Message} - {ex.StackTrace}", "Ok");
+		}
+	}
+}
+
